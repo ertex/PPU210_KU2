32a5ccea2fc91c36649eb7b6fbfa988853376817
ertex <davidlgjohansson@gmail.com>
2019-11-27 16:24:13 +0100

    added vars

--- KU2main.m
+++ KU2main.m
@@ -1,1 +1,10 @@
 %% hej
+
+P = 1; %Power to the system
+v = 1; % wind speed
+h_torn = 3; %m height of the tower
+d_hole = 1; % m holepattern diameter
+t_flange = 1; %flange thickness
+R_a = 7.5e-6; %surface 
+
+c_skruv = 
