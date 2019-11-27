b55b1a2ae5a6eb1e4a75d849183eda50a0ed6a43
ertex <davidlgjohansson@gmail.com>
2019-11-27 16:50:12 +0100

    Most variables added

--- KU2main.m
+++ KU2main.m
@@ -1,10 +1,25 @@
 %% hej
-
+%KU2 grupp 7F
 P = 1; %Power to the system
 v = 1; % wind speed
-h_torn = 3; %m height of the tower
+h_tower = 3; %m height of the tower
 d_hole = 1; % m holepattern diameter
 t_flange = 1; %flange thickness
 R_a = 7.5e-6; %surface 
+n_max = 0.75; %max usable effect from windspeed
+sigma_utm = 30e6; %if and only if bolt >= M36
+my_WB = [0.10 0.40]; %(0.10 - 0.40) Friction between washer and bolt
+my_thread =  [0.07 0.35]; %(0.07-0.35) Frction in the thread
+w_flange = 190e-3; %flange width
+d_tower = 2*(d_hole/2 + 0.065) %Tower outer diameter
+Pitch = 1.5; %Bolt Pitch
 
 c_skruv = 
+
+Mb = F_wind * h_tower;
+
+Mtot = Fax*(0.16*Pitch + 0.58*my_thread  )
+
+
+
+
