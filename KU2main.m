ddf9ff687fba7bd45f2ccbaa8e39ee5c25f50e7b
Emil Nilsson <58263626+Svinto@users.noreply.github.com>
2019-11-27 16:59:16 +0100

    Update KU2main.m

--- KU2main.m
+++ KU2main.m
@@ -2,9 +2,7 @@
 
 P = 1; %Power to the system
 v = 1; % wind speed
-h_torn = 3; %m height of the tower
 d_hole = 1; % m holepattern diameter
 t_flange = 1; %flange thickness
 R_a = 7.5e-6; %surface 
 
-c_skruv = 
